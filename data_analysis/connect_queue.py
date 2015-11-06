7c94cca77bbcf0bf3fa99ca43f5af48c0208b59b
likedan <likedan5@icloud.com>
2015-11-05 21:03:47 -0600

    update

--- data_analysis/connect_queue.py
+++ data_analysis/connect_queue.py
@@ -10,15 +10,23 @@ print beanstalk.watching()
 job = beanstalk.reserve()
 print job.body
 
-# def addJob(id):
-#     beanstalk = beanstalkc.Connection(host='localhost', port=14711)
-#     beanstalk.use('unit_to_update')
-#     beanstalk.put(id)
-#     string = 'update_unit_' + id
-#     beanstalk.watch(string)
-#     job = beanstalk.reserve(timeout=10000)
-#     print job.body
-#
-# for num in range(0, 6):
-#     p = threading.Thread(target=addJob, args=("ZH350127",))
-#     p.start()
+def addJob(id):
+    beanstalk = beanstalkc.Connection(host='localhost', port=14711)
+    beanstalk.use('unit_to_update')
+    beanstalk.put(id)
+    string = 'update_unit_' + id
+    beanstalk.watch(string)
+    job = beanstalk.reserve(timeout=10000)
+    print job.body
+
+for num in range(0, 10):
+    p = threading.Thread(target=addJob, args=("ZH350127",))
+    p.start()
+
+for num in range(0, 13):
+    p = threading.Thread(target=addJob, args=("ZH350327",))
+    p.start()
+
+for num in range(0, 13):
+    p = threading.Thread(target=addJob, args=("ZH453327",))
+    p.start()
