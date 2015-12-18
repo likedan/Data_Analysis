814dfef085bd7c2da7323582fa4bd21d4890afce
likedan <likedan5@icloud.com>
2015-12-18 00:53:11 -0600

    update

--- maxGain/run.py
+++ maxGain/run.py
@@ -4,29 +4,27 @@ import time
 import engine
 
 eng = engine.Engine()
-eng.getNextThreeDaysHighest(1432588511.637, "300339")
 
-def calculateMaxGain(data):
-    t = time.strftime("%Y-%m-%d", time.localtime(float(data["time"])/1000 + 13*60*60))
+def calculateMaxGain(data, engine):
     for stock in data["list"]:
-        stockName = stock.keys()[0]
+        stockName = stock.keys()[0][2:]
         amount = 0
         if stock[stockName]["from_value"] == None:
             amount = stock[stockName]["to_value"]
         elif stock[stockName]["to_value"] - stock[stockName]["from_value"] > 0:
             amount = stock[stockName]["to_value"] - stock[stockName]["from_value"]
         price = stock[stockName]["current_price"]
-        print amount
+        highest = eng.getNextThreeDaysHighest(float(data["time"])/1000.0 + 13*60*60, stockName)
         print price
+        print highest
 
 
+db = database.Database()
+for entry in db.data.find():
+    if "trade_timetable" in entry:
+	print(entry["id"])
+        trade_timetable = entry["trade_timetable"]
+        for trade in trade_timetable:
+            calculateMaxGain(trade)
 
-# db = database.Database()
-# for entry in db.data.find():
-#     if "trade_timetable" in entry:
-# 	print(entry["id"])
-#         trade_timetable = entry["trade_timetable"]
-#         for trade in trade_timetable:
-#             calculateMaxGain(trade)
-#
-# 	print("---------")
+	print("---------")
