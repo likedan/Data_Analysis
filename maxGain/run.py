ac43fc3918d66671029d16a990452777e4f1dcd4
likedan <likedan5@icloud.com>
2015-12-17 23:48:17 -0600

    update maxGain

--- maxGain/run.py
+++ maxGain/run.py
@@ -13,12 +13,13 @@ print stock_obj[0].as_dict()
 def calculateMaxGain(data):
     t = time.strftime("%Y-%m-%d", time.localtime(data["time"]/1000 + 13*60*60))
     for stock in data["list"]:
+        stockName = stock.keys()[0]
         amount = 0
-        if stock["from_value"] == None:
-            amount = stock["to_value"]
-        elif stock["to_value"] - stock["from_value"] > 0:
-            amount = stock["to_value"] - stock["from_value"]
-        price = stock["current_price"]
+        if stock["stockName"]["from_value"] == None:
+            amount = stock["stockName"]["to_value"]
+        elif stock["stockName"]["to_value"] - stock["stockName"]["from_value"] > 0:
+            amount = stock["stockName"]["to_value"] - stock["stockName"]["from_value"]
+        price = stock["stockName"]["current_price"]
         print amount
         print price
 
