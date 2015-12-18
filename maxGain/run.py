d4db1870f9ace3189b6d4636e320d37bc5f90b07
likedan <likedan5@icloud.com>
2015-12-18 00:02:11 -0600

    encapsulate engine

--- maxGain/run.py
+++ maxGain/run.py
@@ -1,14 +1,10 @@
-import sys
-sys.path.append('chinese_stock_api')
-from cstock.request import Requester
-from cstock.yahoo_engine import YahooEngine
+
 import database
 import time
+import engine
 
-engine = YahooEngine()
-requester = Requester(engine)
-stock_obj = requester.request('000626',("2014-03-04","2014-03-06"))
-print stock_obj[0].as_dict()
+eng = engine.Engine()
+eng.getNextThreeDaysHighest("300339","300339")
 
 def calculateMaxGain(data):
     t = time.strftime("%Y-%m-%d", time.localtime(data["time"]/1000 + 13*60*60))
@@ -25,12 +21,12 @@ def calculateMaxGain(data):
 
 
 
-db = database.Database()
-for entry in db.data.find():
-    if "trade_timetable" in entry:
-	print(entry["id"])
-        trade_timetable = entry["trade_timetable"]
-        for trade in trade_timetable:
-            calculateMaxGain(trade)
-
-	print("---------")
+# db = database.Database()
+# for entry in db.data.find():
+#     if "trade_timetable" in entry:
+# 	print(entry["id"])
+#         trade_timetable = entry["trade_timetable"]
+#         for trade in trade_timetable:
+#             calculateMaxGain(trade)
+#
+# 	print("---------")
