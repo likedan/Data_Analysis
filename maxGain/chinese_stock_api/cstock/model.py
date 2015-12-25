2158461bd7865140434f9b4064216044d29bcc04
likedan <likedan5@icloud.com>
2015-12-25 08:21:42 +0800

    add download stock data module

---
+++ maxGain/chinese_stock_api/cstock/model.py
@@ -0,0 +1,58 @@
+#    Copyright (c) 2015 Walt Chen
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+
+
+class ParserException(Exception):
+    pass
+
+
+class Stock(object):
+    
+    # yesterday_close is yesterday close price
+    # close is today close price
+
+    # volume: unit of stock transacted
+    # turnover: total transaction money
+
+    __slots__ = [
+        'name',
+        'code',
+        'date',
+        'time',
+        'price',
+        'open',
+        'close',
+        'high',
+        'low',
+        'volume',
+        'turnover',
+        'yesterday_close',
+    ]
+
+    def __init__(self, **argvs):
+        
+        for (k, v) in argvs.items():
+            setattr(self, k, v)
+
+    def as_dict(self):
+        result = {
+            i: getattr(self, i, None)
+            for i in self.__slots__
+        }
+
+        # dispose date and time because they are datetime class instance
+        if result['date'] is not None:
+            result['date'] = str(result['date'])
+
+        if result['time'] is not None:
+            result['time'] = str(result['time'])
+
+        return result
+
+
+__all__ = ['ParserException', 'Stock']
---
+++ maxGain/run.py
@@ -0,0 +1,32 @@
+
+import database
+import time
+import engine
+
+eng = engine.Engine()
+eng.getNextThreeDaysHighest("300339","300339")
+
+def calculateMaxGain(data):
+    t = time.strftime("%Y-%m-%d", time.localtime(data["time"]/1000 + 13*60*60))
+    for stock in data["list"]:
+        stockName = stock.keys()[0]
+        amount = 0
+        if stock[stockName]["from_value"] == None:
+            amount = stock[stockName]["to_value"]
+        elif stock[stockName]["to_value"] - stock[stockName]["from_value"] > 0:
+            amount = stock[stockName]["to_value"] - stock[stockName]["from_value"]
+        price = stock[stockName]["current_price"]
+        print amount
+        print price
+
+
+
+# db = database.Database()
+# for entry in db.data.find():
+#     if "trade_timetable" in entry:
+# 	print(entry["id"])
+#         trade_timetable = entry["trade_timetable"]
+#         for trade in trade_timetable:
+#             calculateMaxGain(trade)
+#
+# 	print("---------")
