e7f72c3c3e92e2c2a5da886d5d88960298ac5ad2
likedan <likedan5@icloud.com>
2015-12-27 06:59:21 +0800

    generate most features

--- data_analysis/strategies/macd.py
+++ data_analysis/strategies/macd.py
@@ -181,7 +181,7 @@ class MACD:
                 else:
                     will_intersect.append(0)
 
-        for i in xrange(len(will_intersect) - 50):
+        for i in xrange(len(will_intersect) - 1):
             # calculate refuse intersection
             if intersection[i] == 0 and will_intersect[i] == 0 and will_intersect[i + 1] != 0:
                 if will_intersect[i + 1] == -1:
@@ -191,26 +191,14 @@ class MACD:
             else:
                 refuse_intersect.append(0)
 
-            total_gain = 0.0
-
             #calculate the cumulative gain of the next 3 days  local min and local max
             if i < 3:
-                outcome.append(None)
+                local_min.append(0)
+                local_max.append(0)
+            elif i > len(dea) - 3:
                 local_min.append(0)
                 local_max.append(0)
             else:
-                price_range = [float(price_data[i]["Close"])]
-                for x in xrange(3):
-                    price_range.append(float(price_data[i-x-1]["Open"]))
-                    price_range.append(float(price_data[i-x-1]["Close"]))
-
-                for x in xrange(len(price_range) - 1):
-                    value = price_range[x + 1] - price_range[x]
-                    if value < 0:
-                        value = value * discount_for_lost
-                    total_gain = total_gain + value
-                outcome.append(total_gain / price_range[0])
-
                 is_max = True
                 for x in xrange(1,3):
                     if dea[i + x] < dea[i + x - 1] and dea[i - x] < dea[i - x + 1]:
@@ -236,6 +224,28 @@ class MACD:
                     local_min.append(1)
                 else:
                     local_min.append(0)
+
+
+
+        for i in xrange(len(will_intersect) - 50):
+
+            total_gain = 0.0
+
+            #calculate the cumulative gain of the next 3 days
+            if i < 3:
+                outcome.append(None)
+            else:
+                price_range = [float(price_data[i]["Close"])]
+                for x in xrange(3):
+                    price_range.append(float(price_data[i-x-1]["Open"]))
+                    price_range.append(float(price_data[i-x-1]["Close"]))
+
+                for x in xrange(len(price_range) - 1):
+                    value = price_range[x + 1] - price_range[x]
+                    if value < 0:
+                        value = value * discount_for_lost
+                    total_gain = total_gain + value
+                outcome.append(total_gain / price_range[0])
 
             price_range = []
             for x in xrange(50):
@@ -319,7 +329,35 @@ class MACD:
         def save_features():
             for index in xrange(len(outcome)):
                 day_features = {"_id": price_data[index]["Date"]}
-                day_features["outcome"] = outcome[index]
+
+                def get_outcome_value():
+                    if outcome[index] >= -0.015 and outcome[index] <= 0.015:
+                        return 0
+                    elif outcome[index] > 0.015 and outcome[index] <= 0.05:
+                        return 1
+                    elif outcome[index] < -0.015 and outcome[index] >= -0.05:
+                        return -1
+                    elif outcome[index] > 0.05 and outcome[index] <= 0.09:
+                        return 2
+                    elif outcome[index] < -0.05 and outcome[index] >= -0.09:
+                        return -2
+                    elif outcome[index] > 0.09 and outcome[index] <= 0.14:
+                        return 3
+                    elif outcome[index] < -0.09 and outcome[index] >= -0.14:
+                        return -3
+                    elif outcome[index] > 0.14 and outcome[index] <= 0.2:
+                        return 4
+                    elif outcome[index] < -0.14 and outcome[index] >= -0.2:
+                        return -4
+                    elif outcome[index] > 0.2 and outcome[index] <= 0.3:
+                        return 5
+                    elif outcome[index] < -0.2 and outcome[index] >= -0.3:
+                        return -5
+                    elif outcome[index] > 0.3:
+                        return 10
+                    elif outcome[index] < -0.3:
+                        return -10
+                day_features["outcome"] = get_outcome_value()
 
                 def get_intersection_value(day_num):
                     if intersection[day_num] == 0:
@@ -335,37 +373,128 @@ class MACD:
                         else:
                             return -2
 
-                day_features["intersection_day1"] = get_intersection_value(index)
-                day_features["intersection_day2"] = get_intersection_value(index + 1)
-                day_features["intersection_day3"] = get_intersection_value(index + 2)
-                day_features["intersection_day4"] = get_intersection_value(index + 3)
-                day_features["intersection_day5"] = get_intersection_value(index + 4)
+                for i in xrange(10):
+                    string = "intersection_day" + str(i)
+                    day_features[string] = get_intersection_value(index + i)
 
-                day_features["macd_cross_axis_day1"] = macd_cross_axis[index]
-                day_features["macd_cross_axis_day2"] = macd_cross_axis[index + 1]
-                day_features["macd_cross_axis_day3"] = macd_cross_axis[index + 2]
-                day_features["macd_cross_axis_day4"] = macd_cross_axis[index + 3]
-                day_features["macd_cross_axis_day5"] = macd_cross_axis[index + 4]
+                def consecutive_days_features(day_num, array, string):
+                    for i in xrange(day_num):
+                        key = string + str(i)
+                        day_features[key] = array[index + i]
+                consecutive_days_features(10, macd_cross_axis, "macd_cross_axis_day")
+                consecutive_days_features(10, macdsignal_cross_axis, "macdsignal_cross_axis_day")
+                consecutive_days_features(10, refuse_intersect, "refuse_intersect_day")
+                consecutive_days_features(10, will_intersect, "will_intersect_day")
 
-                day_features["macdsignal_cross_axis_day1"] = macdsignal_cross_axis[index]
-                day_features["macdsignal_cross_axis_day2"] = macdsignal_cross_axis[index + 1]
-                day_features["macdsignal_cross_axis_day3"] = macdsignal_cross_axis[index + 2]
-                day_features["macdsignal_cross_axis_day4"] = macdsignal_cross_axis[index + 3]
-                day_features["macdsignal_cross_axis_day5"] = macdsignal_cross_axis[index + 4]
+                cross_count = 0
+                gold_intersect_count = 0
+                death_intersect_count = 0
+                refuse_gold_intersect_count = 0
+                refuse_death_intersect_count = 0
+                for i in xrange(50):
+                    if i == 20:
+                        day_features["cross_count_20"] = cross_count
+                        day_features["gold_intersect_count_20"] = gold_intersect_count
+                        day_features["death_intersect_count_20"] = death_intersect_count
+                        day_features["refuse_gold_intersect_count_20"] = refuse_gold_intersect_count
+                        day_features["refuse_death_intersect_count_20"] = refuse_death_intersect_count
 
-                day_features["refuse_intersect_day1"] = refuse_intersect[index]
-                day_features["refuse_intersect_day2"] = refuse_intersect[index + 1]
-                day_features["refuse_intersect_day3"] = refuse_intersect[index + 2]
-                day_features["refuse_intersect_day4"] = refuse_intersect[index + 3]
-                day_features["refuse_intersect_day5"] = refuse_intersect[index + 4]
+                    if intersection[index + i] == 1:
+                        gold_intersect_count = gold_intersect_count + 1
+                    elif intersection[index + i] == -1:
+                        death_intersect_count = death_intersect_count + 1
 
-                day_features["will_intersect_day1"] = will_intersect[index]
-                day_features["will_intersect_day2"] = will_intersect[index + 1]
-                day_features["will_intersect_day3"] = will_intersect[index + 2]
-                day_features["will_intersect_day4"] = will_intersect[index + 3]
-                day_features["will_intersect_day5"] = will_intersect[index + 4]
+                    if refuse_intersect[index + i] == 1:
+                        refuse_gold_intersect_count = refuse_gold_intersect_count + 1
+                    elif refuse_intersect[index + i] == -1:
+                        refuse_death_intersect_count = refuse_death_intersect_count + 1
 
-                print macd_cross_axis[index]
+                    if macdsignal_cross_axis[index + i] != 0:
+                        cross_count = cross_count + 1
+
+                day_features["cross_count_50"] = cross_count
+                day_features["gold_intersect_count_50"] = gold_intersect_count
+                day_features["death_intersect_count_50"] = death_intersect_count
+                day_features["refuse_gold_intersect_count_50"] = refuse_gold_intersect_count
+                day_features["refuse_death_intersect_count_50"] = refuse_death_intersect_count
+
+                day_features["stock_3_slope"] = stock_3_slope[index]
+                day_features["stock_3_cov"] = stock_3_cov[index]
+                day_features["stock_3_pval"] = stock_3_pval[index]
+                day_features["stock_3_err"] = stock_3_err[index]
+
+                day_features["stock_8_slope"] = stock_8_slope[index]
+                day_features["stock_8_cov"] = stock_8_cov[index]
+                day_features["stock_8_pval"] = stock_8_pval[index]
+                day_features["stock_8_err"] = stock_8_err[index]
+
+                day_features["stock_20_slope"] = stock_20_slope[index]
+                day_features["stock_20_cov"] = stock_20_cov[index]
+                day_features["stock_20_pval"] = stock_20_pval[index]
+                day_features["stock_20_err"] = stock_20_err[index]
+
+                day_features["stock_50_slope"] = stock_50_slope[index]
+                day_features["stock_50_cov"] = stock_50_cov[index]
+                day_features["stock_50_pval"] = stock_50_pval[index]
+                day_features["stock_50_err"] = stock_50_err[index]
+
+                day_features["dea_3_slope"] = dea_3_slope[index]
+                day_features["dea_3_cov"] = dea_3_cov[index]
+                day_features["dea_3_pval"] = dea_3_pval[index]
+                day_features["dea_3_err"] = dea_3_err[index]
+
+                day_features["dea_8_slope"] = dea_8_slope[index]
+                day_features["dea_8_cov"] = dea_8_cov[index]
+                day_features["dea_8_pval"] = dea_8_pval[index]
+                day_features["dea_8_err"] = dea_8_err[index]
+
+                day_features["dea_20_slope"] = dea_20_slope[index]
+                day_features["dea_20_cov"] = dea_20_cov[index]
+                day_features["dea_20_pval"] = dea_20_pval[index]
+                day_features["dea_20_err"] = dea_20_err[index]
+
+                day_features["dea_50_slope"] = dea_50_slope[index]
+                day_features["dea_50_cov"] = dea_50_cov[index]
+                day_features["dea_50_pval"] = dea_50_pval[index]
+                day_features["dea_50_err"] = dea_50_err[index]
+
+                day_features["dif_3_slope"] = dif_3_slope[index]
+                day_features["dif_3_cov"] = dif_3_cov[index]
+                day_features["dif_3_pval"] = dif_3_pval[index]
+                day_features["dif_3_err"] = dif_3_err[index]
+
+                day_features["dif_8_slope"] = dif_8_slope[index]
+                day_features["dif_8_cov"] = dif_8_cov[index]
+                day_features["dif_8_pval"] = dif_8_pval[index]
+                day_features["dif_8_err"] = dif_8_err[index]
+
+                day_features["dif_20_slope"] = dif_20_slope[index]
+                day_features["dif_20_cov"] = dif_20_cov[index]
+                day_features["dif_20_pval"] = dif_20_pval[index]
+                day_features["dif_20_err"] = dif_20_err[index]
+
+                day_features["dif_50_slope"] = dif_50_slope[index]
+                day_features["dif_50_cov"] = dif_50_cov[index]
+                day_features["dif_50_pval"] = dif_50_pval[index]
+                day_features["dif_50_err"] = dif_50_err[index]
+
+                day_features["stock_dif_3_slope"] = stock_3_slope[index] - dif_3_slope[index]
+                day_features["stock_dif_8_slope"] = stock_8_slope[index] - dif_8_slope[index]
+                day_features["stock_dif_20_slope"] = stock_20_slope[index] - dif_20_slope[index]
+                day_features["stock_dif_50_slope"] = stock_50_slope[index] - dif_50_slope[index]
+
+                day_features["stock_dea_3_slope"] = stock_3_slope[index] - dea_3_slope[index]
+                day_features["stock_dea_8_slope"] = stock_8_slope[index] - dea_8_slope[index]
+                day_features["stock_dea_20_slope"] = stock_20_slope[index] - dea_20_slope[index]
+                day_features["stock_dea_50_slope"] = stock_50_slope[index] - dea_50_slope[index]
+
+                day_features["dif_dea_3_slope"] = dif_3_slope[index] - dea_3_slope[index]
+                day_features["dif_dea_8_slope"] = dif_8_slope[index] - dea_8_slope[index]
+                day_features["dif_dea_20_slope"] = dif_20_slope[index] - dea_20_slope[index]
+                day_features["dif_dea_50_slope"] = dif_50_slope[index] - dea_50_slope[index]
+
+                print day_features
+                # print day_features
             # self.features[self.stockID].update({"_id": entry["_id"]}, entry, upsert=True)
         save_features()
 
