f10577484f46e667e6e8196f2594d1a640516fba
likedan <likedan5@icloud.com>
2016-01-04 14:59:40 +0800

    change number

--- mine_user/start_crawl.py
+++ mine_user/start_crawl.py
@@ -12,13 +12,13 @@ print(db.get_greatest_amoung(400000,599999))
 print(db.get_greatest_amoung(600000,799999))
 print(db.get_greatest_amoung(800000,999999))
 
-db.print_amoung(0,200000)
+# db.print_amoung(0,200000)
 
 def crawl(start, end):
     my_crawler = crawler.Crawler()
     info = my_crawler.check_unit_existance(start, end)
 
-p = Process(target=crawl, args=(15713, 100000))
+p = Process(target=crawl, args=(216143, 39999))
 p.start()
 # #
 # for x in xrange(5):
