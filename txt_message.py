57ec837fd17a8f40603ed30a494eec9ba8360b30
likedan <likedan5@icloud.com>
2016-03-14 09:12:34 -0500

    update token

--- txt_message.py
+++ txt_message.py
@@ -10,7 +10,7 @@ from pyvirtualdisplay import Display
 
 def send_message(message):
 	account_sid = "ACd4c3fbbe5af185b06bce7e1032adcd56"
-	auth_token = "4117b759bae2e5d74ea414b6290fe237"
+	auth_token = "a49fa8be35fde08211b453bb0cc9433a"
 	client = TwilioRestClient(account_sid, auth_token) 
 	client.messages.create(to="+12175080347", from_="+12176864502", body=message)
 
