64b36c771b835953e8456462b8e199ba7abd2146
Kedan Li <likedan5@icloud.com>
2016-04-25 14:57:07 -0500

    add exception handler

--- realtime_checker/stock_scanner_all.py
+++ realtime_checker/stock_scanner_all.py
@@ -22,33 +22,37 @@ symbols = helper.get_local_symbol_list()
 
 def scan_stocks(date1, date2, symbols, thread_num):
 	while True:
-		for s in symbols:
-			try:
-				quotes = helper.get_data_from_file(s)
-				stock_opening, stock_high, stock_low, stock_closing, stock_vol = helper.get_today_total(symbol=s)
-				quote_with_date = ((quotes[-1][0] + 1), stock_opening, stock_high, stock_low, stock_closing, stock_vol)
-				if datetime.now().weekday() == 1:
-					#is monday
-					quote_with_date = ((quotes[-1][0] + 3), stock_opening, stock_high, stock_low, stock_closing, stock_vol)
-				print "Thread"+thread_num + ":   " + s
-				quotes.append(quote_with_date)
+		try:
+			for s in symbols:
+				try:
+					quotes = helper.get_data_from_file(s)
+					stock_opening, stock_high, stock_low, stock_closing, stock_vol = helper.get_today_total(symbol=s)
+					quote_with_date = ((quotes[-1][0] + 1), stock_opening, stock_high, stock_low, stock_closing, stock_vol)
+					if datetime.now().weekday() == 1:
+						#is monday
+						quote_with_date = ((quotes[-1][0] + 3), stock_opening, stock_high, stock_low, stock_closing, stock_vol)
+					print "Thread"+thread_num + ":   " + s
+					quotes.append(quote_with_date)
 
 
-			except Exception as e:
-				print e
-				continue
+				except Exception as e:
+					print e
+					continue
 
-			stock_opening = [quotes[i][1] for i in xrange(len(quotes))]
-			stock_high = [quotes[i][2] for i in xrange(len(quotes))]
-			stock_low = [quotes[i][3] for i in xrange(len(quotes))]
-			stock_closing = [quotes[i][4] for i in xrange(len(quotes))]
-			stock_vol = [quotes[i][5] for i in xrange(len(quotes))]
+				stock_opening = [quotes[i][1] for i in xrange(len(quotes))]
+				stock_high = [quotes[i][2] for i in xrange(len(quotes))]
+				stock_low = [quotes[i][3] for i in xrange(len(quotes))]
+				stock_closing = [quotes[i][4] for i in xrange(len(quotes))]
+				stock_vol = [quotes[i][5] for i in xrange(len(quotes))]
 
-			lhv_arr, lhv_index = candleStickScanner.scan_low_with_huge_vol(stock_opening, stock_closing, stock_high, stock_low, stock_vol)
-			lhv_con_arr, lhv_con_index = candleStickScanner.scan_low_with_huge_vol_consecutive(stock_opening, stock_closing, lhv_arr, separate_by_price_moving_range=True)
+				lhv_arr, lhv_index = candleStickScanner.scan_low_with_huge_vol(stock_opening, stock_closing, stock_high, stock_low, stock_vol)
+				lhv_con_arr, lhv_con_index = candleStickScanner.scan_low_with_huge_vol_consecutive(stock_opening, stock_closing, lhv_arr, separate_by_price_moving_range=True)
 
-			if lhv_con_arr[-1] == 1:
-				drawCandle.draw_candle_stick_with_today(s, date1, date2, quotes, additional_function=drawCandle.show_result, data=lhv_con_index, name="L")
+				if lhv_con_arr[-1] == 1:
+					drawCandle.draw_candle_stick_with_today(s, date1, date2, quotes, additional_function=drawCandle.show_result, data=lhv_con_index, name="L")
+
+		except Exception as e:
+			print e
 
 p = threading.Thread(target=scan_stocks, args=(date1, date2, symbols, "for",))
 p.start()
