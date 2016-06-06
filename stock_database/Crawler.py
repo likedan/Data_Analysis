de947ea1e6b2c34b4d77200671244f64f683900a
Kedan Li <likedan5@icloud.com>
2016-06-05 23:14:56 -0700

    add loading_stock_data

--- stock_database/Crawler.py
+++ stock_database/Crawler.py
@@ -49,25 +49,44 @@ class Crawler:
 
     def download_historical_data(self, stock_url):
 
-        delay = 5
-        full_url = DEFAULT_SITE_URL + os.path.join(stock_url, PRICE_HISTORY_SUFFIX)
-        print full_url
-        self.driver.get(full_url)
-        try:
-            WebDriverWait(self.driver, delay).until(lambda the_driver: the_driver.find_element_by_class_name('qm_history_startMonth').is_displayed())
-            
-            day_input = self.driver.find_element_by_class_name('qm_history_startDay')
-            day_input.clear()
-            day_input.send_keys("1")
+        delay = 10
+        def download_page(page):
+            full_url_page_1 = DEFAULT_SITE_URL + os.path.join(stock_url, PRICE_HISTORY_SUFFIX) + TIMEFRAME_URL + page
+            print full_url_page_1
+            self.driver.get(full_url_page_1)
+            try:
+                WebDriverWait(self.driver, delay).until(lambda the_driver: the_driver.find_element_by_class_name('qm_maintext').is_displayed())
+                source = lxml.html.fromstring(self.driver.page_source)
 
-            year_input = self.driver.find_element_by_class_name('qm_history_startYear')
-            year_input.clear()
-            year_input.send_keys("2000")
-            self.driver.find_element_by_class_name('qm_history_startMonth').send_keys("Jan")
-            # self.driver.find_element_by_class_name('qm_historyTab_GoButton').click()
-        except Exception as e:
-            print "no stock data"
-        
+                stock_price_data = []
+                def add_rows(class_key):
+                    for row in source.xpath('.//table[@class="qm_history_historyContent"]//tbody//tr[@class="' + class_key + '"]'):
+                        one_day_data = {}
+                        one_day_data["date"] = row.xpath('.//td/text()')[0]
+                        one_day_data["open"] = row.xpath('.//td/text()')[1]
+                        one_day_data["high"] = row.xpath('.//td/text()')[2]
+                        one_day_data["low"] = row.xpath('.//td/text()')[3]
+                        one_day_data["close"] = row.xpath('.//td/text()')[4]
+                        one_day_data["volume"] = row.xpath('.//td/text()')[5]
+                        one_day_data["chg"] = row.xpath('.//td/text()')[6]
+                        one_day_data["chg_p"] = row.xpath('.//td/text()')[7]
+                        one_day_data["adj_close"] = row.xpath('.//td/text()')[8]
+                        one_day_data["trade_val"] = row.xpath('.//td/text()')[9]
+                        one_day_data["trades"] = row.xpath('.//td/text()')[10]
+                        stock_price_data.append(one_day_data)
+                add_rows("qm_cycle qm_historyData_row")
+                add_rows("qm_main qm_historyData_row")
+                print len(stock_price_data)
+
+            except Exception as e:
+                print "no stock data"
+                print e
+        download_page("1")
+        download_page("2")
+
+
+
+
 
     def quit(self):
         self.driver.quit()
--- stock_database/DefaultVariables.py
+++ stock_database/DefaultVariables.py
@@ -1,5 +1,6 @@
 DEFAULT_SITE_URL = "http://www.investorpoint.com"
 PRICE_HISTORY_SUFFIX = "price-history"
+TIMEFRAME_URL = "/?startDay=1&startMonth=0&startYear=1995&endDay=5&endMonth=5&endYear=2016&perPage=2500&page="
 
 STOCK_MARKET_URLS = {
     "AMEX": "http://www.investorpoint.com/exchange/AMX-NYSE+Alternext+-+formerly+AMEX/equity/",
