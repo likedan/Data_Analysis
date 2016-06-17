74a26c67cc39fba32ee1bf9ab6635c44f3dee64b
Kedan Li <likedan5@icloud.com>
2016-06-17 12:01:05 -0700

    update database scheme

--- currency/run.py
+++ currency/run.py
@@ -22,14 +22,20 @@ for file_dir in symbol_files:
 	symbol = file_dir[-6:]
 	csv_files = [os.path.join(file_dir, folder) for folder in os.listdir(file_dir) if folder[-4:] == ".csv"]
 	print symbol
-	data = []
+	day_diction = {}
 	for csv_file in csv_files:
+		print csv_file
 		with open(csv_file) as text_file:
 			lines = text_file.readlines()
 			for l in lines:
 				line = l.split(" ")
 				info = line[1].split(';')
-				data.append({"date": int(line[0]), "day_time": int(info[0]), "price":float(info[1])})
+				if not (int(line[0]) in day_diction):
+					day_diction[int(line[0])] = []
+				day_diction[int(line[0])].append([int(info[0]), float(info[1])])
+	data = []
+	for key in day_diction.keys():
+		data.append({"date":key, "timeline": day_diction[key]})
 	db.db[symbol].insert_many(data)
 
 
