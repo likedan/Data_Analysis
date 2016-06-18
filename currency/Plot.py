bacfa126d29020f6780a63c0a8d31b18f546d843
Kedan Li <likedan5@icloud.com>
2016-06-18 08:59:51 -0700

    plot candlestick

--- currency/Plot.py
+++ currency/Plot.py
@@ -6,9 +6,10 @@ import threading
 import zipfile
 import time, os, sys, datetime
 import matplotlib.pyplot as plt
+from matplotlib.finance import candlestick_ohlc
 
 db = Database()
-data = db.get_range_stock_date("EURUSD", "20160607", "20160608")
+data = db.get_range_stock_date("EURUSD", "20140606", "20140610")
 
 time = []
 last = []
@@ -25,11 +26,31 @@ for diction_index in range(len(data)):
 				low.append(last[-1])
 		else:
 			time.append(minute_Price[index]["minute_count"] + MINUTES_PER_DAY * diction_index)
-			last.append(minute_Price[index]["last"])
-			high.append(minute_Price[index]["high"])
-			low.append(minute_Price[index]["low"])
+			last.append(minute_Price[index]["last"] * 1000)
+			high.append(minute_Price[index]["high"] * 1000)
+			low.append(minute_Price[index]["low"] * 1000)
 
-def plot_rgb_range()
+open_data = last[:-1]
+open_data.insert(0, low[0])
+high_data = high
+low_data = low
+close_data = last
+dates = time
+
+price = []
+for index in range(len(dates)):
+	price.append((dates[index],open_data[index],high_data[index],low_data[index],close_data[index]))
+
+#and then following the official example. 
+fig, ax = plt.subplots()
+fig.subplots_adjust(bottom=0.2)
+candlestick_ohlc(ax, price, width=0.6)
+
+ax.xaxis_date()
+ax.autoscale_view()
+plt.show()
+
+def plot_rgb_range():
 	plt.plot(time, low, marker='o', linestyle='--', color='b', label='Low')
 	plt.plot(time, high, marker='o', linestyle='--', color='r', label='High')
 	plt.plot(time, last, marker='o', linestyle='--', color='g', label='Last')
