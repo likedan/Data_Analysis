01c5e12a19270fe33d5764d4a89ad3c09ac1fe2c
Kedan Li <likedan5@icloud.com>
2016-06-18 01:03:14 -0700

    add tick count

--- currency/run.py
+++ currency/run.py
@@ -12,19 +12,21 @@ available_currency_list = db.get_available_currency_list()
 
 for currency in available_currency_list:
 	count = 0
+	print currency
 	for day in db.db[currency].find():
 		count = count + 1
-		# if not ("minute_price_high" in day):
+		#if not ("minute_price_high" in day):
 		date = str(day["date"])
 		unix_time = int(time.mktime(datetime.datetime.strptime(date, "%Y%m%d").timetuple()))
-		minute_dict = [{"minute_count": index, "high": 0, "low": 9999} for index in range(0, 1440)]
+		minute_dict = [{"minute_count": index, "high": 0, "low": 9999, "last": 0, "tick_count": 0} for index in range(0, 1440)]
 		for tick in day["timeline"]:
 			current_minute = tick["adjusted_time"] / 60
 			if tick["price"] > minute_dict[current_minute]["high"]:
 				minute_dict[current_minute]["high"] = tick["price"]
 			if tick["price"] < minute_dict[current_minute]["low"]:
 				minute_dict[current_minute]["low"] = tick["price"]
-			minute_dict[current_minute]["last"] = tick["price"] 
+			minute_dict[current_minute]["last"] = tick["price"]
+			minute_dict[current_minute]["tick_count"] = minute_dict[current_minute]["tick_count"] + 1
 		day["minute_price"] = minute_dict
 		db.db[currency].update({"date": day["date"]}, day, False)
 		print count
