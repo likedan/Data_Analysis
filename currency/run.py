1aedbdb40a39c846523dc7a37e7c1402fb1d9094
Kedan Li <likedan5@icloud.com>
2016-06-22 01:16:27 -0700

    remove overhead not finished

--- currency/run.py
+++ currency/run.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import operator
 import Plot
 import numpy as np
+import math
 
 db = Database()
 currency_data = db.get_one_day_currency_data("EURUSD", 20160609)
@@ -38,17 +39,19 @@ for slice_price in currency_data["minute_price"]:
 	opening.append(slice_price["first"])
 	close.append(slice_price["last"])
 
-frame_size = 100
+frame_size = 50
 frame = currency_data["minute_price"][-frame_size:]
 close = close[-frame_size:]
 high = high[-frame_size:]
 low = low[-frame_size:]
 opening = opening[-frame_size:]
 
-max_cross_rate = 0.05
-associate_tolerance_rate = 0.1
+no_overhead_start_num = 5
+
+max_cross_rate = 0.1
+associate_tolerance_rate = 0.05
 average_range = np.sum(np.absolute(np.array(close) - np.array(opening))) / float(frame_size)
-tolerance_value = associate_tolerance_rate * average_range
+tolerance_value = associate_tolerance_rate * average_range * math.sqrt(math.sqrt(frame_size))
 print tolerance_value
 max_cross_size = frame_size * max_cross_rate
 
@@ -70,26 +73,36 @@ for e_index in reversed(range(frame_size)):
 		for line in current_lines:
 			line.right_end = e_index
 			line.left_end = s_index
-			lines[line] = {"intercept_num": 0, "cross_num" : 0, "line": line}
+			lines[line] = {"intercept_num": 0, "cross_num" : 0, "over_num": 0, "line": line, "intercept_list": [e_index]}
 
-		for test_index in reversed(range(s_index - 1)):
+		for test_index in reversed(range(e_index - 1)):
 			for line in current_lines:
 				if line.point_on_line(test_index, high[test_index],tolerance_value) or line.point_on_line(test_index, max(opening[test_index], close[test_index]),tolerance_value):
 					lines[line]["intercept_num"] += 1
+					lines[line]["intercept_list"].append(test_index)
 					line.left_end = test_index
-				y_val = line.get_y(test_index)
-				if y_val >= min(opening[test_index], close[test_index]) + tolerance_value / 2 and y_val <= max(opening[test_index], close[test_index]) - tolerance_value / 2:
-					lines[line]["cross_num"] += 1
+
+
 
 line_array = []
 for key in lines.keys():
 	line_array.append(lines[key])
 
-#remove overcross line
-for line in reversed(line_array):
-	if line["cross_num"] > max_cross_size:
-		line_array.remove(line)
+#remove overcross line  and overhead
+for test_index in range(frame_size):
+	for line in reversed(line_array):
+		y_val = line["line"].get_y(test_index)
+		if y_val >= min(opening[test_index], close[test_index]) + tolerance_value and y_val <= max(opening[test_index], close[test_index]) - tolerance_value:
+			line["cross_num"] += 1
+		if line["cross_num"] > max_cross_size:
+			line_array.remove(line)
+			break
+		if frame_size - test_index < no_overhead_start_num and min(opening[test_index], close[test_index]) > y_val + tolerance_value:
+			print line
+			line_array.remove(line)
 
+
+line_array = sorted(line_array, key=lambda k: k['intercept_num'])
 #remove duplicate of lines
 for line_index in range(len(line_array)):
 	for l_index in reversed(range(line_index + 1,len(line_array))):
@@ -97,20 +110,23 @@ for line_index in range(len(line_array)):
 			# print str(line_array[line_index]["line"]) + "   " + str(line_array[l_index]["line"]
 			del line_array[l_index]
 
-sorted_lines = sorted(line_array, key=lambda k: k['intercept_num'])
+#calculate interval value
+for line in line_array:
+	product_sum = 1
+	for index in range(len(line["intercept_list"]) - 1):
+		product_sum = product_sum * (line["intercept_list"][index] - line["intercept_list"][index + 1])
+	#ranking modifier
+	line["product_sum"] = product_sum * line["intercept_num"] / (line["cross_num"] + 1) / (line["cross_num"] + 1) / (line["over_num"] + 1)
+
+sorted_lines = sorted(line_array, key=lambda k: k['product_sum'])
 # print sorted_lines
 good_lines = []
-bad_lines = []
-for l in sorted_lines:
-	bad_lines.append(l["line"])
-	if len(bad_lines) == 5: 
-		break
 for l in reversed(sorted_lines):
-	good_lines.append(l["line"])
+	good_lines.append([l["line"]])
 	print l
-	if len(good_lines) == 10: 
+	if len(good_lines) == 7: 
 		break
-Plot.plot_day_candle(frame, currency_data["unix_time"], lines=[good_lines,bad_lines])
+Plot.plot_day_candle(frame, currency_data["unix_time"], lines=good_lines)
 		
 # l = Line(0.3,0.7,1.802,3.93)
 # printl.get_y(0.3)
