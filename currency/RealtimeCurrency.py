65f58d3818f92d15d97143afe6431cb9024aeaed
Kedan Li <likedan5@icloud.com>
2016-06-25 22:06:15 -0700

    realtime currency different store

--- currency/RealtimeCurrency.py
+++ currency/RealtimeCurrency.py
@@ -21,15 +21,17 @@ if len(sys.argv) == 2:
 
 while True:
     for symbol in streaming_currency_list:
-        url = 'http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=EURUSD=X'
-        response = urllib2.urlopen(url, headers={'User-Agent': 'Mozilla/5.0'})
-        cr = csv.reader(response)
-        price = 0
-        for row in cr:
-            price =  row[1]
+        URL = "http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=" + symbol + "=X"
+        request = urllib2.Request(URL, headers={"Accept" : "text/html", 'User-Agent': 'Mozilla/5.0'})
+        contents = urllib2.urlopen(request).read()
+        price = float(contents.split(",")[1])
         current_time = time.time()
+        minute = int(current_time) / 60
         symbol_cache = db.realtime_data.find_one({"symbol": symbol})
-        symbol_cache["data"].append([current_time, price]) 
+        if len(symbol_cache["data"]) > 0 and symbol_cache["data"][-1]["minute"] == minute:
+            symbol_cache["data"][-1]["minute_data"].append(price)
+        else:
+            symbol_cache["data"].append({"minute":minute, "minute_data":[price]})
         db.realtime_data.update({"symbol": symbol}, symbol_cache, False)
-        time.sleep(1)
+    time.sleep(1)
 
