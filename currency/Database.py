d2c808d769a28d3863f3cc56fcddd8ebea3397f8
Kedan Li <likedan5@icloud.com>
2016-06-27 17:57:14 -0700

    simple running strategy

--- currency/Database.py
+++ currency/Database.py
@@ -17,6 +17,12 @@ class Database:
         self.currency_list = self.db['currency_list']
         self.realtime_data = self.db["realtime_data"]
 
+    def get_realtime_Data(self, symbol, length):
+        data = self.realtime_data.find_one({"symbol": symbol})["data"]
+        if len(data) > length:
+            return data[-length:]
+        return None
+
     def get_currency_list(self):
         c_list = [currency for currency in self.currency_list.find()]
         return c_list
--- currency/run.py
+++ currency/run.py
@@ -15,13 +15,31 @@ from SupportResistance import compute_support_resistance
 from TradingView import TradingView
 from PIL import Image
 import urllib2
+db = Database()
+# trading = TradingView()
+# trading.login()
 
-trading = TradingView()
-trading.login()
-while True:
-	trading.trade_up()
-	print "up"
-	time.sleep(5)
-	trading.trade_down()
-	print "down"
-	time.sleep(5)
+
+price_data = db.get_realtime_Data("EURUSD", 25)
+latest_price = price_data[-1]["minute_data"][-1][1]
+
+close = []
+high = []
+low = []
+opening = []
+
+for minute in price_data:
+	transpose_arr = np.array(minute["minute_data"]).T
+	opening.append(minute["minute_data"][0][1])
+	close.append(minute["minute_data"][-1][1])
+	high.append(transpose_arr[1].max())
+	low.append(transpose_arr[1].min())
+
+resistance_lines, support_lines = compute_support_resistance(opening[:-1], high[:-1], low[:-1], close[:-1])
+# while True:
+# 	trading.trade_up()
+# 	print "up"
+# 	time.sleep(5)
+# 	trading.trade_down()
+# 	print "down"
+# 	time.sleep(5)
