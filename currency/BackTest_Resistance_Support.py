6e6a0f838666086d185cedf8ad2ef957f8d10154
Kedan Li <likedan5@icloud.com>
2016-06-30 13:45:16 -0700

    prune data for backtest resistance support

--- currency/BackTest_Resistance_Support.py
+++ currency/BackTest_Resistance_Support.py
@@ -12,50 +12,110 @@ import Plot
 import numpy as np
 import math
 from SupportResistance import compute_support_resistance, parse_historical_data
+from matplotlib.dates import date2num
+
+start_time = 20160203
+end_time = 20160503
 
 db = Database()
-currency_data = db.get_range_currency_date("EURUSD", 20160203 ,20160503)
+currency_data = db.get_range_currency_date("EURUSD", start_time ,end_time)
+suppose_unix_time = int(time.mktime(datetime.datetime.strptime(str(start_time), "%Y%m%d").timetuple()))
+serialized_chunk = [[]]
+
 for day_data in currency_data:
-	frame_size = 26
+	print day_data["unix_time"]
+	if day_data["unix_time"] != suppose_unix_time:
+		serialized_chunk.append([])
+		suppose_unix_time = day_data["unix_time"]
+		print "  "
 
-	for index in range(len(day_data["minute_price"])):
-		frame = []
-		# for frame_size in range(20,30):
-		frame, opening, high, low, close = parse_historical_data(day_data["minute_price"][:-index-1], frame_size = frame_size)
-		if len(frame) < frame_size:
+	serialized_chunk[-1] = serialized_chunk[-1] + day_data["minute_price"]
+	suppose_unix_time += SECONDS_PER_DAY
+
+print serialized_chunk[-1]
+
+for chunk_index in range(len(serialized_chunk)):
+	start_index = 0
+	for minute_data in serialized_chunk[chunk_index]:
+		if minute_data["tick_count"] == 0:
+			start_index += 1
+		else:
 			break
+	end_index = len(serialized_chunk[chunk_index])
+	for minute_data in reversed(serialized_chunk[chunk_index]):
+		if minute_data["tick_count"] == 0:
+			end_index -= 1
+		else:
+			break
+	serialized_chunk[chunk_index] = serialized_chunk[chunk_index][start_index: end_index]
+	print start_index
+	print end_index
+print "!!??"
+print serialized_chunk[-1]
 
-		resistance_lines, support_lines = compute_support_resistance(opening[:-2], high[:-2], low[:-2], close[:-2])
-		support_end_points = []
-		good_support = []
-		support_slope = []
-		for l in reversed(support_lines[-7:]):
-			good_support.append(l["line"])
-			support_slope.append(l["line"].slope)
-			support_end_points.append(l["line"].get_y(0))
-			support_end_points.append(l["line"].get_y(frame_size))
-		good_resisitance = []
-		resistance_slope = []
+	# frame_size = 25
+	
+	# frame = []
+	# support_slope_arr = []
+	# resistance_slope_arr = []
+	# # for frame_size in range(20,30):
+	# frame, opening, high, low, close = parse_historical_data(day_data["minute_price"], frame_size = frame_size)
+	# resistance_lines, support_lines = compute_support_resistance(opening[:-1], high[:-1], low[:-1], close[:-1])
 
-		resistance_end_points = []
-		for l in reversed(resistance_lines[-7:]):
-			good_resisitance.append(l["line"])
-			resistance_slope.append(l["line"].slope)
-			resistance_end_points.append(l["line"].get_y(0))
-			resistance_end_points.append(l["line"].get_y(frame_size))
+	# support_end_points = []
+	# good_support = []
+	# support_slope = []
+	# for l in reversed(support_lines[-7:]):
+	# 	good_support.append(l["line"])
+	# 	support_slope.append(l["line"].slope)
+	# 	support_end_points.append(l["line"].get_y(0))
+	# 	support_end_points.append(l["line"].get_y(frame_size))
+	# good_resisitance = []
+	# resistance_slope = []
 
-		normalize_slope_val = max(resistance_end_points) - min(support_end_points) 
-		resistance_slope.remove(max(resistance_slope))
-		resistance_slope.remove(min(resistance_slope))
-		support_slope.remove(max(support_slope))
-		support_slope.remove(min(support_slope))
+	# resistance_end_points = []
+	# for l in reversed(resistance_lines[-7:]):
+	# 	good_resisitance.append(l["line"])
+	# 	resistance_slope.append(l["line"].slope)
+	# 	resistance_end_points.append(l["line"].get_y(0))
+	# 	resistance_end_points.append(l["line"].get_y(frame_size))
 
-		good_lines = []
-		for index in range(3):
-			good_lines.append([good_support[index], good_resisitance[index]])
-		good_lines[2][0].slope = (good_lines[0][0].slope+good_lines[1][0].slope)/2
-		good_lines[2][0].intercept = (good_lines[0][0].intercept+good_lines[1][0].intercept)/2
-		good_lines[2][1].slope = (good_lines[0][1].slope+good_lines[1][1].slope)/2
-		good_lines[2][1].intercept = (good_lines[0][1].intercept+good_lines[1][1].intercept)/2
-		# Plot.plot_day_candle(frame, day_data["unix_time"], "EURUSD", lines=good_lines, save=True)
-		print "compute"
+	# normalize_slope_val = max(resistance_end_points) - min(support_end_points) 
+	# resistance_slope.remove(max(resistance_slope))
+	# resistance_slope.remove(min(resistance_slope))
+	# support_slope.remove(max(support_slope))
+	# support_slope.remove(min(support_slope))
+
+	# # support_slope_arr.append((np.std(np.array(support_slope)), good_support))
+	# # resistance_slope_arr.append((np.std(np.array(resistance_slope)), good_resisitance))
+	# print str(day_data["date"]) + "  " +  str(np.std(np.array(resistance_slope) / normalize_slope_val)) + "  " + str(np.std(np.array(support_slope) / normalize_slope_val))
+
+	# good_support_arr = sorted(support_slope_arr, key=lambda x: x[0])
+	# good_resisitance_arr = sorted(resistance_slope_arr, key=lambda x: x[0])
+	# # for index in range(5):
+	# # 	good_support = good_support_arr[index][1]
+	# # 	good_resisitance = good_resisitance_arr[index][1]
+
+	# good_lines = []
+	# for index in range(5):
+	# 	good_lines.append([good_support[index], good_resisitance[index]])
+	# # good_lines[2][0].slope = (good_lines[0][0].slope+good_lines[1][0].slope)/2
+	# # good_lines[2][0].intercept = (good_lines[0][0].intercept+good_lines[1][0].intercept)/2
+	# # good_lines[2][1].slope = (good_lines[0][1].slope+good_lines[1][1].slope)/2
+	# # good_lines[2][1].intercept = (good_lines[0][1].intercept+good_lines[1][1].intercept)/2
+	# dates = []
+	# last = []
+	# high = []
+	# low = []
+	# first = []
+	# for index in range(len(frame)):
+	# 	if frame[index]["tick_count"] == 0:
+	# 		pass
+	# 	else:
+	# 		last.append(frame[index]["last"])
+	# 		high.append(frame[index]["high"])
+	# 		low.append(frame[index]["low"])
+	# 		first.append(frame[index]["first"])
+	# 		dates.append(datetime.datetime.fromtimestamp(day_data["unix_time"] + frame[index]["minute_count"] * 60))
+
+	# Plot.plot_day_candle(dates, first, high, low, last, "EURUSD", lines=good_lines, save=True)
