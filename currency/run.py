ccc00fd705b44a2fda0e768671d79052b8dc27e4
Kedan Li <likedan5@icloud.com>
2016-07-13 17:13:52 -0700

    training with boiling

--- currency/run.py
+++ currency/run.py
@@ -25,15 +25,27 @@ symbol = "EURUSD"
 start_time = 20160223
 end_time = 20160223
 db = Database()
+training_data = []
+training_result = []
+
 currency_data = db.get_range_currency_date(symbol, start_time ,end_time)
 raw_training_data = Helper.get_ML_data_for_resistance_support(currency_data, symbol = symbol, start_time = start_time, end_time = end_time)
 for chunk in raw_training_data:
 	unixtime, opening, high, low, close, good_result = chunk
-	mean_average5 = Indicators.compute_moving_average(high, 5)
-	mean_average14 = Indicators.compute_moving_average(high, 14)
-	center, up, down = Indicators.compute_bollinger_bands(close, 14, 2)
+
+	mean_average5 = Indicators.compute_moving_average(close, 5)
+	mean_average9 = Indicators.compute_moving_average(close, 9)
+	mean_average20 = Indicators.compute_moving_average(close, 20)
+	mean_average50 = Indicators.compute_moving_average(close, 50)
+
+	mean_average5 = [0 for x in range(len(opening) - len(mean_average5))] + mean_average5
+
+	center, outer_up, outer_down = Indicators.compute_bollinger_bands(close, 14, 2)
+	center, inner_up, inner_down = Indicators.compute_bollinger_bands(close, 14, 1)
+
 	print len(center)
-	print len(opening)
+	print len(mean_average5)
+	print mean_average5
 	Plot.plot_day_candle(Helper.unix_to_date_object(unixtime), opening, high, low, close, symbol, start_index=[13, 13, 13], lines=[center, up, down])
 	# print mean_average3
 	# print close
